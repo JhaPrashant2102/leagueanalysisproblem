4e4b84d5f314262a8735e99a517e9e83b6935aa0
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-10-27 10:12:18 +0530

    added feature to extract top batting average and also provided test cases

---
+++ src/main/java/com/cg/leagueanalysisproblem/IplAnalyserException.java
@@ -0,0 +1,26 @@
+package com.cg.leagueanalysisproblem;
+
+/**
+ * @author Prashant
+ *custom exception for league analysis problem
+ */
+public class IplAnalyserException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public enum ExceptionType{
+		INCORRECT_CLASS_TYPE,INCORRECT_DELIMITER,INCORRECT_HEADER, CENSUS_FILE_PROBLEM, NO_DATA, EMPTY_LIST;
+	}
+	
+	public ExceptionType type;
+
+	public IplAnalyserException(ExceptionType type) {
+		super();
+		this.type = type;
+	}
+
+	public IplAnalyserException(String message,ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+	
+}
